7061e5fe7570ec63945ac7736427cefa66e03153
Ang Long <aisk1988@gmail.com>
2017-03-01 19:02:55 +0800

    make 马甸桥 great again

--- scripts/elder.js
+++ scripts/elder.js
@@ -2,6 +2,22 @@
 
 const 钦点 = require('random-weighted-choice');
 
+const 领导班子名单 = [
+  {weight: 10, id: '金城牛肉面'},
+  {weight: 10, id: '重庆小面'},
+  {weight: 1, id: '鸭血粉丝汤'},
+  {weight: 2, id: '马甸清真寺'},
+  {weight: 20, id: '桂林米粉'},
+  {weight: 10, id: '煎饼果子'},
+  {weight: 5, id: '请点外卖'},
+  {weight: 5, id: '黄焖鸡米饭'},
+  {weight: 5, id: '卤煮'},
+  {weight: 5, id: '重庆小面全国五十强'},
+  {weight: 3, id: '江西菜'},
+  {weight: 5, id: '请点外卖'},
+  {weight: 1, id: '铁锅焖面'}
+];
+
 module.exports = function(robot) {
   let poem = '苟利国家生死以岂因福祸避趋之';
 
@@ -26,19 +42,30 @@ module.exports = function(robot) {
   });
 
   robot.hear(/吃/, function(res) {
-    res.send(`**${钦点([
-      {weight: 10, id: '金城牛肉面'},
-      {weight: 10, id: '重庆小面'},
-      {weight: 1, id: '鸭血粉丝汤'},
-      {weight: 2, id: '马甸清真寺'},
-      {weight: 20, id: '桂林米粉'},
-      {weight: 10, id: '煎饼果子'},
-      {weight: 5, id: '请点外卖'},
-      {weight: 1, id: '不，王滨老师，我们不吃铁锅焖面'}
-    ])}**`);
+    res.send(`**${钦点(领导班子名单)}**`);
+  });
+
+  robot.hear(/名单/, function(res) {
+    res.send(JSON.stringify(领导班子名单));
   });
 
   robot.hear(/焖面/, function(res) {
     res.send('不，王滨老师，我们不吃铁锅焖面');
+  });
+
+  var 行不行的算法 = function(res) {
+    if (Math.random() > 0.5) {
+      res.send('这不清真，阿拉胡阿克巴！');
+    } else {
+      res.send('可以，这很清真👌。');
+    }
+  }
+
+  robot.hear('行', 行不行的算法);
+
+  robot.hear('算法', function(res) {
+    res.send('行不行的算法是:\n' +
+      '```js\n' + 行不行的算法.toString() + '```'
+    );
   });
 }
